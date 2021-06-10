fd0f64bd79315cd6ea2a990e270e05fd276c5972
mohammadaminyza <mohamad.aminyaghoobzadeh@gmail.com>
2021-06-10 15:11:44 +0430

    Add LauncherManager

---
+++ Jarvis.VoiceAssistant/Jarvis.VoiceAssistant.Android/DependencyServices/LauncherManager.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Jarvis.VoiceAssistant.DependencyServices;
+using Jarvis.VoiceAssistant.Droid.DependencyServices;
+using Environment = System.Environment;
+
+[assembly: Xamarin.Forms.Dependency(typeof(LauncherManager))]
+namespace Jarvis.VoiceAssistant.Droid.DependencyServices
+{
+    class LauncherManager : ILauncherManager
+    {
+        public string GetRoot()
+        {
+            var backingFile = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyMusic), "Music/");
+
+            return backingFile;
+        }
+    }
+}
---
+++ Jarvis.VoiceAssistant/Jarvis.VoiceAssistant/DependencyServices/ILauncherManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jarvis.VoiceAssistant.DependencyServices
+{
+    public interface ILauncherManager
+    {
+        string GetRoot();
+    }
+}
